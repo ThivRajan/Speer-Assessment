f734e0a7389e41e8847e48f9b71fa94d7765d55a
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-03-11 20:30:39 -0500

    Use styled component props to handle condition CSS

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -6,26 +6,20 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Menu = ({ bg, fgOpen, fgClosed, highlight }) => {
 
-	const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false)
 	const menuItems = ['what is it', 'perks', 'pricing']
 
 	return (
 		<MenuContainer isOpen={isOpen} bg={bg} fgOpen={fgOpen} fgClosed={fgClosed}
 			highlight={highlight}
 		>
-			<div className={`circle ${isOpen ? 'open' : 'closed'}`}></div>
+			<div className='circle'></div>
 			<span className="menu-head">
 				<FontAwesomeIcon icon={faBars} onClick={() => setIsOpen(!isOpen)} />
 				<span>EXP|CON</span>
 			</span>
 			<div className="menu-body">
-				{
-					menuItems.map(item =>
-						<div className={`menu-item ${isOpen ? 'visible' : 'hidden'}`}>
-							{item}
-						</div>
-					)
-				}
+				{menuItems.map(item => <div className='menu-item'>{item}</div>)}
 			</div>
 		</MenuContainer>
 	)
@@ -33,6 +27,9 @@ const Menu = ({ bg, fgOpen, fgClosed, highlight }) => {
 
 const MenuContainer = styled.div`
 	font-size: 20px;
+	position: absolute;
+	top: 10px;
+	left: 10px;
 
 	.circle {
 		position: absolute;
@@ -42,17 +39,8 @@ const MenuContainer = styled.div`
 		background: ${props => props.bg};
 		z-index: -1;
 		transition: width 350ms ease-in, height 350ms ease-in;
-	}
 
-	.open {
-		width: 200px;
-		height: 200px;
-
-	}
-
-	.closed {
-		width: 0px;
-		height: 0px;
+		${props => props.isOpen ? 'width: 200px; height: 200px' : 'width: 0px; height: 0px'};
 	}
 
 	.menu-head {
@@ -68,15 +56,8 @@ const MenuContainer = styled.div`
 		font-weight: 300;
 		text-transform: uppercase;
 		color: ${props => props.fgOpen};
+		opacity: ${props => props.isOpen ? '1' : '0'};
 		transition: opacity 500ms;
-	}
-
-	.visible {
-		opacity: 1;
-	}
-
-	.hidden {
-		opacity: 0;
 	}
 `
 
